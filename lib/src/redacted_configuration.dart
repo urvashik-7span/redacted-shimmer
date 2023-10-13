4290c4f0575fb8049b658be140b24d4ba1651878
urvashikharecha <urvashi.k@7span.com>
2023-10-13 12:23:13 +0530

    pass Color parameter to constructor

--- lib/src/redacted_configuration.dart
+++ lib/src/redacted_configuration.dart
@@ -3,14 +3,13 @@ import 'package:flutter/material.dart';
 class RedactedConfiguration {
   bool autoFillTexts;
   String autoFillText;
-  Color? redactedColor = Colors.grey.shade400.withAlpha(100);
+  Color redactedColor;
   Duration animationDuration;
 
   RedactedConfiguration({
-    this.autoFillText =
-        "This is a place holder text to fill the space for redaction",
+    this.autoFillText = "This is a place holder text to fill the space for redaction",
     this.autoFillTexts = true,
     this.animationDuration = const Duration(milliseconds: 800),
-    this.redactedColor,
+    this.redactedColor = const Color(0xffE0E0E2),
   });
 }
